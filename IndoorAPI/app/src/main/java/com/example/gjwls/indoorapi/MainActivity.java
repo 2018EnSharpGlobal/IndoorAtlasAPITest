9d8741c9d5898d0894d1b7291581ed5132222eb7
Jean-Gyu Hu <gjwlsrb1700@gmail.com>
2018-08-08 16:42:48 +0900

    feat: IndoorAPI example

--- IndoorAPI/app/src/main/java/com/example/gjwls/indoorapi/MainActivity.java
+++ IndoorAPI/app/src/main/java/com/example/gjwls/indoorapi/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.gjwls.indoorapi;
 
+import android.Manifest;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -12,12 +14,14 @@ import com.indooratlas.android.sdk.IALocationRequest;
 
 public class MainActivity extends AppCompatActivity {
 
+    private final int CODE_PERMISSIONS = 0;//...
+
     IALocationManager mLocationManager;
 
     IALocationListener mLocationListener = new IALocationListener() {
         @Override
         public void onLocationChanged(IALocation iaLocation) {
-            TextView txtLoc = (TextView) findViewById(R.id.textView);
+            TextView txtLoc = (TextView)findViewById(R.id.textView);
             Log.e(this.getClass().getName(),String.valueOf("---------------------------------"+iaLocation.getLatitude() +","+iaLocation.getLongitude()));
             txtLoc.setText(String.valueOf(iaLocation.getLatitude() +","+iaLocation.getLongitude()));
         }
@@ -25,6 +29,37 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onStatusChanged(String s, int i, Bundle bundle) {
 
+            switch (i) {
+                case IALocationManager.STATUS_CALIBRATION_CHANGED:
+                    String quality = "unknown";
+                    switch (bundle.getInt("quality")) {
+                        case IALocationManager.CALIBRATION_POOR:
+                            quality = "Poor";
+                            break;
+                        case IALocationManager.CALIBRATION_GOOD:
+                            quality = "Good";
+                            break;
+                        case IALocationManager.CALIBRATION_EXCELLENT:
+                            quality = "Excellent";
+                            break;
+                    }
+
+//                    TextView txtLoc = (TextView)findViewById(R.id.textView);
+//                    txtLoc.setText(String.valueOf("Calibration change. Quality: " + quality));
+                    Log.e(this.getClass().getName(),String.valueOf("Calibration change. Quality: " + quality));
+                    break;
+                case IALocationManager.STATUS_AVAILABLE:
+                    Log.e(this.getClass().getName(),String.valueOf("onStatusChanged: Available"));
+                    break;
+                case IALocationManager.STATUS_LIMITED:
+                    Log.e(this.getClass().getName(),String.valueOf("onStatusChanged: Limited"));
+                    break;
+                case IALocationManager.STATUS_OUT_OF_SERVICE:
+                    Log.e(this.getClass().getName(),String.valueOf("onStatusChanged: Out of service"));
+                    break;
+                case IALocationManager.STATUS_TEMPORARILY_UNAVAILABLE:
+                    Log.e(this.getClass().getName(),String.valueOf("onStatusChanged: Temporarily unavailable"));
+            }
         }
     };
     @Override
@@ -32,20 +67,37 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        String[] neededPermissions = {
+                Manifest.permission.CHANGE_WIFI_STATE,
+                Manifest.permission.ACCESS_WIFI_STATE,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+        };
+        ActivityCompat.requestPermissions( this, neededPermissions, CODE_PERMISSIONS );
+
         mLocationManager = IALocationManager.create(this);
     }
+    @Override
     protected void onResume(){
         super.onResume();
         mLocationManager.requestLocationUpdates(IALocationRequest.create(),mLocationListener);
     }
 
+    @Override
     protected void onPause(){
-        mLocationManager.removeLocationUpdates(mLocationListener);
         super.onPause();
+        mLocationManager.removeLocationUpdates(mLocationListener);
     }
 
+    @Override
     protected void onDestroy(){
-        mLocationManager.destroy();
         super.onDestroy();
+        mLocationManager.destroy();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        //Handle if any of the permissions are denied, in grantResults
     }
 }
